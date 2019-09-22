88a2364fb655f176f9c9aac15da537ff12dd3e7f
Felipe Cadavid <felipe@digifit.eu>
2019-09-21 21:56:41 -0500

    First Commit

---
+++ src/main/java/com/medstory/MedstoryApplication.java
@@ -0,0 +1,12 @@
+package com.medstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedstoryApplication.class, args);
+    }
+}
